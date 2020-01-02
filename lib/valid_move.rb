242a62fe11db7368d9afe4f36b6825cf4e185e5d
Miles Higbie <miles.higbie@gmail.com>
2020-01-02 23:19:23 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,13 @@
 # code your #valid_move? method here
 
+def position_taken?(board,index)
+    if board[index] == " " || board[index] == "" || board[index] == nil 
+       return false 
+    elsif board[index] == "X" || board[index] == "O"
+       return true 
+    end 
+end 
+
 def valid_move(board,index)
   
 end 
